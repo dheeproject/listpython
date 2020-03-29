fccda420d7cd29fd637d242a90ecddf4175cc4d1
dheeproject <62594025+dheeproject@users.noreply.github.com>
2020-03-29 17:17:33 +0530

    firstadded

---
+++ first list program.py
@@ -0,0 +1,8 @@
+lst1=[]
+n=int(input("enter the number of elements you want to enter in list"))
+print("ok!Enter them\n")
+for i in range(0,n):
+    new=int(input())
+    lst1.append(new)
+print("\nyour entered list is")
+print(lst1)
